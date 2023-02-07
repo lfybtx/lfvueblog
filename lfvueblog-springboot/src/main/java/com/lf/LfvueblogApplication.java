731a399adbc2d0c576e0a831771f4f58f646ae36
lfybtx <1278560674@qq.com>
2023-02-07 20:21:11 +0800

    add two domain

--- lfvueblog-springboot/src/main/java/com/lf/LfvueblogApplication.java
+++ lfvueblog-springboot/src/main/java/com/lf/LfvueblogApplication.java
@@ -1,5 +1,6 @@
 package com.lf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
