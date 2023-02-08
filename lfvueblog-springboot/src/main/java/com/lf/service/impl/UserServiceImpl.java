ea5ab5c4ecbb8fabf4b86bcaa5af1aa7d59180c5
lfybtx <1278560674@qq.com>
2023-02-08 16:13:34 +0800

    完善UserDetails

--- lfvueblog-springboot/src/main/java/com/lf/service/impl/UserServiceImpl.java
+++ lfvueblog-springboot/src/main/java/com/lf/service/impl/UserServiceImpl.java
@@ -1,11 +1,31 @@
 package com.lf.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lf.dao.UserDao;
+import com.lf.domain.LoginUser;
 import com.lf.domain.User;
 import com.lf.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
-public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService {
+public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService , UserDetailsService {
+        @Override
+        public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+                //查询用户信息
+                LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper<>();
+                queryWrapper.eq(User::getUsername,s);
+                User user = getOne(queryWrapper);
+                if(Objects.isNull(user)){
+                        throw new RuntimeException("用户名或密码错误");
+                }
+                //TODO 查询对应权限信息
+                //封装成UserDetails对象返回
+                return new LoginUser(user);
+        }
 }
--- lfvueblog-springboot/src/test/java/com/lf/LfvueblogApplicationTests.java
+++ lfvueblog-springboot/src/test/java/com/lf/LfvueblogApplicationTests.java
@@ -1,13 +1,22 @@
 package com.lf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
+@Slf4j
 class LfvueblogApplicationTests {
 
+
+        @Autowired
+        private PasswordEncoder passwordEncoder;
         @Test
         void contextLoads() {
+                String encode = passwordEncoder.encode("123456");
+                log.info(encode);
         }
 
 }
